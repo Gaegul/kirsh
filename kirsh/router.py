ce9191b021f638ab6d82d0228873efe60a6766d6
sineunju <eunjuoi0515@gmail.com>
2020-07-06 14:02:54 +0900

    [ADD] 옷가게 랭킹 api

--- kirsh/router.py
+++ kirsh/router.py
@@ -10,3 +10,6 @@ api_basic.add_resource(SignUp, "/signup")
 
 from kirsh.view.auth import Login
 api_basic.add_resource(Login, "/login")
+
+from kirsh.view.ranking import Ranking
+api_basic.add_resource(Ranking, "/ranking")
