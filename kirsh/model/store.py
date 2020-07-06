075f7f568597e2a006859a212350281eee9436b2
sineunju <eunjuoi0515@gmail.com>
2020-07-06 11:44:31 +0900

    [ADD] MODEL 정의

---
+++ kirsh/model/store.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, String, Integer
+
+from kirsh.model import Base
+
+
+class Store(Base):
+    __tablename__ = 'store'
+    _table_args_ = {'mysql_collate': 'utf8_general_ci'}
+
+    id = Column(String(100), primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(200), nullable=False)
+    score = Column(Integer, nullable=True)
+    average_score = Column(Integer, nullable=False)
+    ranking = Column(Integer, nullable=True)
